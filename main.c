80fe55932a8ea6a69d57314eec942fdaa54ed334
Koenig-Heinrich-der-4te <84435773+Koenig-Heinrich-der-4te@users.noreply.github.com>
2022-11-01 17:13:03 +0100

    fixed string comparison and str to int conversion

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "prototypes.h"
 
 // Flags
@@ -7,30 +8,36 @@ int devmode = 0;
 int godmode = 0;
 int logging_level = 0;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	// Reading program arguments
-	if (argc>0) {
-		int doNotCheckNext = 0;
-		for (int i = 1; i < argc; i++) {
-			if (doNotCheckNext == 1) { 
-				doNotCheckNext = 0; 
-				continue; 
+	if (argc > 0)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			if (strcmp(argv[i], "-d") == 0)
+			{
+				devmode = 1;
 			}
-			if (argv[i] == "-d") { devmode = 1; }
-			else if (argv[i] == "-g") { godmode = 1; }
-			else if (argv[i] == "-l") { 
-				logging_level = (int)(argv[i+1]); 
-				doNotCheckNext = 1;
+			else if (strcmp(argv[i], "-g") == 0)
+			{
+				godmode = 1;
+			}
+			else if (strcmp(argv[i], "-l") == 0)
+			{
+				if (i + 1 < argc)
+				{
+					logging_level = (int)strtol(argv[++i], (char **)NULL, 10);
+				}
 			}
 		}
 	}
 
 	// Verifying validity of arguments
-	if (logging_level > 2 || logging_level < 0) {
+	if (logging_level > 2 || logging_level < 0)
+	{
 		return -1;
 	}
+
 	return 0;
 }
-
-
-
